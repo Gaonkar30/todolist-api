5fdab04e9f131e30c6253296575e0afb5e72712c
Gaonkar30 <akashggaonkar@gmail.com>
2024-07-10 09:56:42 +0530

    better controller file

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
-const Task = require("../models/tasks");
+const User = require("../models/usermodel"); // Fix this path if incorrect
+const Task = require("../models/taskmodel"); // Fix this path if incorrect
 const jwt = require("jsonwebtoken");
+
 const salt = bcrypt.genSaltSync(10);
-const secret="asdfjioefr4io"
+const secret = "asdfjioefr4io";
+
 const register = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -14,13 +16,17 @@ const register = async (req, res) => {
     });
     res.json(userdoc);
   } catch (e) {
-    res.json(e);
+    res.status(500).json(e);
   }
 };
-const Login = async (req, res) => {
+
+const login = async (req, res) => {
   try {
     const { username, password } = req.body;
     const userdoc = await User.findOne({ username });
+    if (!userdoc) {
+      return res.status(400).json("Invalid credentials");
+    }
     const userok = bcrypt.compareSync(password, userdoc.password);
     if (userok) {
       jwt.sign({ username, id: userdoc._id }, secret, {}, (err, token) => {
@@ -30,44 +36,67 @@ const Login = async (req, res) => {
           username: userdoc.username,
         });
       });
+    } else {
+      res.status(400).json("Invalid credentials");
     }
   } catch (err) {
-    res.json(err);
+    res.status(500).json(err);
   }
 };
-const Logout=async(req,res)=>{
-    res.cookie('token','').json('ok');
-}
-const createTask=async(req,res)=>{
-    try{
-        const{task,status,date}=req.body;
-        jwt.verify(token,secret,{},(err,info)=>{
-            if(err) throw err;
-            return Task.create({
-                task,
-                status,
-                date,
-                user:info.id,
-            });
-            res.json('task created');
-        })
-        
-    }
-    catch(e){
-        res.json(e);
-    }
-}
-const getTasks=async(req,res)=>{
-    res.json(await Task.find().populate('user',['username'])).limit(10)
+
+const logout = async (req, res) => {
+  res.cookie("token", "").json("ok");
 };
 
-const updateTask=async(req,res)=>{
-    jwt.verify(token,secret,{},async(err,info)=>{
-        if(err) throw err;
-        const {id}=req.params;
-        const{task,status,date}=req.body;
-        const taskdoc=await Task.findOne({_id:id});
-        await taskdoc.updateOne({task,status,date});
+const createTask = async (req, res) => {
+  try {
+    const { task, status, date } = req.body;
+    const token = req.cookies.token; // Add this line
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const taskdoc = await Task.create({
+        task,
+        status,
+        date,
+        userid: info.id, // Changed to match schema
+      });
+      res.json(taskdoc);
+    });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+const getTasks = async (req, res) => {
+  res.json(await Task.find().populate("userid", ["username"]).limit(10));
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const token = req.cookies.token; // Add this line
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const { id } = req.params;
+      const { task, status, date } = req.body;
+      const taskdoc = await Task.findById(id);
+      if (taskdoc.userid.toString() === info.id) {
+        // Verify task ownership
+        await taskdoc.updateOne({ task, status, date });
         res.json(taskdoc);
-    })
-}
+      } else {
+        res.status(403).json("Unauthorized");
+      }
+    });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+  createTask,
+  getTasks,
+  updateTask,
+};
