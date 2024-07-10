b16f40fc95143b94937f706770ac522e8490967d
Gaonkar30 <akashggaonkar@gmail.com>
2024-07-10 09:59:07 +0530

    added cookieParser

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const todoRoutes = require("./routes/todoRoutes");
+const cookieParser = require("cookie-parser");
 require("./config/todo-config");
+
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser()); 
 app.use("/api", todoRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
