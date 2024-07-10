515b88d044e5c7c75aba5f4fe2e5436cdcca6e63
Gaonkar30 <akashggaonkar@gmail.com>
2024-07-10 09:57:52 +0530

    fixed updateTask route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,16 +1,20 @@
-const express=require('express');
-const {register,
-    Login,
-    Logout,
-    createTask,
-    getTasks,
-    updateTask
-}=require('../controllers/taskControllers');
-const router=express.Router();
-router.post('/register',register);
-router.post('/login',Login);
-router.post('/logout',Logout);
-router.post('/createTask',createTask);
-router.get('/getTasks',getTasks);
-router.put('/updateTask',updateTask);
-module.exports=router;
+const express = require('express');
+const {
+  register,
+  login,
+  logout,
+  createTask,
+  getTasks,
+  updateTask,
+} = require('../controllers/taskcontroller'); // Fix this path if incorrect
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/createTask', createTask);
+router.get('/getTasks', getTasks);
+router.put('/updateTask/:id', updateTask); // Add :id for task identification
+
+module.exports = router;
